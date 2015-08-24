7a4fd01e22e5399ea29046e56012c8309968913c
maurovillena <maurovillena>
2015-08-24 17:30:40 -0400

    jeje

--- page-obras.php
+++ page-obras.php
@@ -6,66 +6,8 @@
 	<div class="row">
 		<div class="col-sm-8 col-sm-offset-2">
 
-			<ul><?php
-				$agoras = get_posts(array(
-					'post_type'	    => 'page',
-					'category_name'	=> 'agoras',
-					'orderby'		=> 'name',
-					'order'         => 'asc'
-				));
-				foreach($agoras as $link){
-					echo '<li><a href="'.get_page_link($link->ID).'">'.$link->post_title.'</a></li>';
-				}
-			?></ul>
 
-			<ul><?php
-				$esculturas = get_posts(array(
-					'post_type'	    => 'page',
-					'category_name'	=> 'esculturas',
-					'orderby'		=> 'name',
-					'order'         => 'asc'
-				));
-				foreach($esculturas as $link){
-					echo '<li><a href="'.get_page_link($link->ID).'">'.$link->post_title.'</a></li>';
-				}
-			?></ul>
-
-			<ul><?php
-				$obraspublicas = get_posts(array(
-					'post_type'	    => 'page',
-					'category_name'	=> 'obras-publicas',
-					'orderby'		=> 'name',
-					'order'         => 'asc'
-				));
-				foreach($obraspublicas as $link){
-					echo '<li><a href="'.get_page_link($link->ID).'">'.$link->post_title.'</a></li>';
-				}
-			?></ul>
-
-			<ul><?php
-				$hospederias = get_posts(array(
-					'post_type'	    => 'page',
-					'category_name'	=> 'hospederias',
-					'orderby'		=> 'name',
-					'order'         => 'asc'
-				));
-				foreach($hospederias as $link){
-					echo '<li><a href="'.get_page_link($link->ID).'">'.$link->post_title.'</a></li>';
-				}
-			?></ul>
-
-			<ul><?php
-				$publicas = get_posts(array(
-					'post_type'	    => 'page',
-					'category_name'	=> 'obras-publicas',
-					'orderby'		=> 'name',
-					'order'         => 'asc'
-				));
-				foreach($publicas as $link){
-					echo '<li><a href="'.get_page_link($link->ID).'">'.$link->post_title.'</a></li>';
-				}
-			?></ul>
-		<!--	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
 			<?php	if ( has_post_thumbnail() ) {
 			echo '<div class="jumbo-foto">'.'<h1>'.get_the_title().'</h1>'.get_the_post_thumbnail().'</div>';
@@ -84,7 +26,77 @@
 
 				<article class="entry-content">
 					<?php echo the_content(); ?>
-				</article>-->
+				</article>
+
+				<h3>Ágoras</h3>
+
+			<ul><?php
+				$agoras = get_posts(array(
+					'post_type'	    => 'page',
+					'category_name'	=> 'agoras',
+					'orderby'		=> 'name',
+					'order'         => 'asc'
+				));
+				foreach($agoras as $link){
+					echo '<li><a href="'.get_page_link($link->ID).'">'.$link->post_title.'</a></li>';
+				}
+			?></ul>
+
+			<h3>Esculturas</h3>
+
+			<ul><?php
+				$esculturas = get_posts(array(
+					'post_type'	    => 'page',
+					'category_name'	=> 'esculturas',
+					'orderby'		=> 'name',
+					'order'         => 'asc'
+				));
+				foreach($esculturas as $link){
+					echo '<li><a href="'.get_page_link($link->ID).'">'.$link->post_title.'</a></li>';
+				}
+			?></ul>
+
+			<h3>Obras Públicas</h3>
+
+			<ul><?php
+				$obraspublicas = get_posts(array(
+					'post_type'	    => 'page',
+					'category_name'	=> 'obras-publicas',
+					'orderby'		=> 'name',
+					'order'         => 'asc'
+				));
+				foreach($obraspublicas as $link){
+					echo '<li><a href="'.get_page_link($link->ID).'">'.$link->post_title.'</a></li>';
+				}
+			?></ul>
+
+			<h3>Hospederías y Vestales</h3>
+
+			<ul><?php
+				$hospederias = get_posts(array(
+					'post_type'	    => 'page',
+					'category_name'	=> 'hospederias',
+					'orderby'		=> 'name',
+					'order'         => 'asc'
+				));
+				foreach($hospederias as $link){
+					echo '<li><a href="'.get_page_link($link->ID).'">'.$link->post_title.'</a></li>';
+				}
+			?></ul>
+
+			<h3>Salas y Talleres</h3>
+
+			<ul><?php
+				$salas = get_posts(array(
+					'post_type'	    => 'page',
+					'category_name'	=> 'salas-y-talleres',
+					'orderby'		=> 'name',
+					'order'         => 'asc'
+				));
+				foreach($publicas as $link){
+					echo '<li><a href="'.get_page_link($link->ID).'">'.$link->post_title.'</a></li>';
+				}
+			?></ul>
 				
 				<!-- GeoMashup -->
 				<div id="geomashup">
