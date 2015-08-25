b24a534388fce55df79385bf838b4fe79482bc8f
maurovillena <maurovillena>
2015-08-25 17:03:23 -0400

    ble

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php get_header(); ?>
+
+		<div class="col-sm-6 hidden-xs">
+			<?php dynamic_sidebar( 'wide' ); ?>
+			<?php dynamic_sidebar( 'clean' ); ?>
+			<?php get_sidebar(); ?>
+		</div>
+
+
+		<div class="col-sm-6">
+			<section id="content" role="main" class='row'>
+				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); 
+				get_template_part( 'entry-small' );
+				comments_template();
+				endwhile; endif; ?>
+				<?php get_template_part( 'nav', 'below' ); ?>
+			</section>
+		</div>
+
+
+<?php get_footer(); ?>
+
+
+
