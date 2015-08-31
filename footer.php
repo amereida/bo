12c20852782c160b5d91322f9fddd92acd7d6d20
Herbert Spencer <herbert.spencer@gmail.com>
2015-08-30 23:50:18 -0300

    footer

--- footer.php
+++ footer.php
@@ -1,9 +1,15 @@
 
 <footer id="footer" role="contentinfo">
 	
-	<div class="container">		
-		<?php dynamic_sidebar( 'footer-left' ); ?>
-		<?php dynamic_sidebar( 'footer-right' ); ?>
+	<div class="container">	
+		<div class='row'>
+			<div class="col-md-10 col-md-offset-1">
+				<div class="row">
+					<?php dynamic_sidebar( 'footer-left' ); ?>
+					<?php dynamic_sidebar( 'footer-right' ); ?>
+				</div>
+			</div>
+		</div>
 	</div>
 		
 </footer>
